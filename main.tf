40a7dae624e74e991a20c4e13ee8fa5ceca98a5d
Chinmaya Kumar Padhi <chinmaya.kumar.padhi@effem.com>
2024-02-10 10:17:21 +0000

    first commit

---
+++ main.tf
@@ -0,0 +1,21 @@
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+}
+
+# Create a resource group
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
